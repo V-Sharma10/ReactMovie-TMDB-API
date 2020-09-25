f16a52e3efb812028ed8e62e40fd74c6565997c4
V-Sharma10 <2017ugcs071@nitjsr.ac.in>
2020-09-25 17:40:18 +0530

    serviceWorker registeres

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.register();
